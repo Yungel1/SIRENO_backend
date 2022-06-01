e13b813ab52e46a8f37c7544b89a15bbb89c1e59
Jonpor13 <jonpor13@gmail.com>
2022-06-01 15:45:20 +0200

    Activar activación completado

--- src/controllers/activacion.controllers.js
+++ src/controllers/activacion.controllers.js
@@ -138,14 +138,6 @@ exports.actualizarActivacion = async function (req,res,next){
                 message: "La activación que se intenta actualizar no existe",
             });
         }
-        
-        var idCamapñaEsInt = HelperNumeric.isNumeric(idCampaña);
-        //Comprobar si el id de la campaña es un numero
-        if (!idCamapñaEsInt){
-            return res.status(422).json({
-                message: "La campaña seleccionada no es un número",
-            });
-        }       
 
         var noFechaValida = (!moment(fechaActIni, 'YYYY-MM-DD',true).isValid() || !moment(fechaActFin, 'YYYY-MM-DD',true).isValid());
          //Comprobar si el formato de fechas es correcto
@@ -178,3 +170,102 @@ exports.actualizarActivacion = async function (req,res,next){
         return res.sendStatus(500) && next(err);
     }
 }
+
+//Activar activacion admin
+exports.activarActivacionAdmin = async function (req,res,next){
+    try{
+
+        var idDocente = req.body.idDocente;
+        var idGrupo = req.body.idGrupo;
+        var idGrado = req.body.idGrado;
+        var idAsignatura = req.body.idAsignatura;
+        var idCampaña = req.body.idCampaña;
+        var activado = req.body.activado;
+
+        var activacionExiste = await ActivacionService.activacionExiste(idDocente, idGrupo, idGrado, idAsignatura, idCampaña);
+        //Comprobar si la activacion existe
+        if(!activacionExiste){
+            return res.status(201).json({
+                message: "La activación que se intenta activar no existe",
+            });
+        }
+
+        var activadoEsCeroOUno = ([0,1].includes(Number(activado)));
+        //Comprobar si son integer (0 o 1) false y true
+        if(!activadoEsCeroOUno){
+            return res.status(422).json({
+                message: "No ha insertado un valor entre 0 y 1 en los campos correspondientes",
+            });
+        }
+
+        var actualizado = await ActivacionService.activarActivacion(idDocente, idGrupo, idGrado, idAsignatura, idCampaña, activado); //Activar la activación
+
+        //Comprobar si se ha actualizado la activacion
+        if(actualizado){
+            return res.status(201).json({
+                message: "La activación ha sido activada correctamente",
+            });
+        } else{
+            return res.status(422).json({
+                message: "La activación no ha sido activada",
+            });
+        }
+    } catch(err){
+        console.log(err);
+        return res.sendStatus(500) && next(err);
+    }
+}
+
+//Activar activacion docente
+exports.activarActivacionDocente = async function (req,res,next){
+    try{
+
+        var idDocente = req.usuario;
+        var idGrupo = req.body.idGrupo;
+        var idGrado = req.body.idGrado;
+        var idAsignatura = req.body.idAsignatura;
+        var idCampaña = req.body.idCampaña;
+        var activado = req.body.activado;
+
+        var docenteExiste = await UsuarioService.docenteExiste(idDocente)
+        //Comprobar si el id es de un docente
+        if(! docenteExiste){
+            return res.status(422).json({
+                message: "El usuario con el ha iniciado sesión no corresponde a ningún docente.",
+            });
+        }
+
+        var activacionExiste = await ActivacionService.activacionExiste(idDocente, idGrupo, idGrado, idAsignatura, idCampaña);
+        //Comprobar si la activacion existe
+        if(!activacionExiste){
+            return res.status(201).json({
+                message: "La activación que se intenta activar no existe",
+            });
+        }
+        
+        var activadoEsCeroOUno = ([0,1].includes(Number(activado)));
+        //Comprobar si son integer (0 o 1) false y true
+        if(!activadoEsCeroOUno){
+            return res.status(422).json({
+                message: "No ha insertado un valor entre 0 y 1 en los campos correspondientes",
+            });
+        }
+
+        var actualizado = await ActivacionService.activarActivacion(idDocente, idGrupo, idGrado, idAsignatura, idCampaña, activado); //Activar la activación
+
+        //Comprobar si se ha actualizado la activacion
+        if(actualizado){
+            return res.status(201).json({
+                message: "La activación ha sido activada correctamente",
+            });
+        } else{
+            return res.status(422).json({
+                message: "La activación no ha sido activada",
+            });
+        }
+    } catch(err){
+        console.log(err);
+        return res.sendStatus(500) && next(err);
+    }
+}
+
