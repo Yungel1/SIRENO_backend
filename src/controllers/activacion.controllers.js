66c80570f9c617597e0fad0f9c5bbfd17b6aba3e
Jonpor13 <jonpor13@gmail.com>
2022-06-01 15:07:52 +0200

    Mejoras en el codigo

--- src/controllers/activacion.controllers.js
+++ src/controllers/activacion.controllers.js
@@ -13,7 +13,16 @@ var HelperNumeric = require('../helpers/helperNumeric.js');
 exports.insertarActivacion = async function (req,res,next){
     try{
 
-        var activacionExiste = await ActivacionService.activacionExiste(req.body.docente, req.body.grupo, req.body.grado, req.body.asignatura, req.body.campaña);
+        var idDocente = req.body.idDocente;
+        var idGrupo = req.body.idGrupo;
+        var idGrado = req.body.idGrado;
+        var idAsignatura = req.body.idAsignatura;
+        var idCampaña = req.body.idCampaña;
+        var fechaActIni = req.body.fechaActIni;
+        var fechaActFin = req.body.fechaActFin;
+
+
+        var activacionExiste = await ActivacionService.activacionExiste(idDocente, idGrupo, idGrado, idAsignatura, idCampaña);
         //Comprobar si la activacion existe
         if(activacionExiste){
             return res.status(201).json({
@@ -21,7 +30,7 @@ exports.insertarActivacion = async function (req,res,next){
             });
         }
 
-        var usuarioExiste = await UsuarioService.usuarioExiste(req.body.docente);
+        var usuarioExiste = await UsuarioService.usuarioExiste(idDocente);
         //Comprobar si el usuario existe
         if(! usuarioExiste){
             return res.status(201).json({
@@ -29,7 +38,7 @@ exports.insertarActivacion = async function (req,res,next){
             });
         }
        
-        var docenteExiste = await UsuarioService.docenteExiste(req.body.docente)
+        var docenteExiste = await UsuarioService.docenteExiste(idDocente)
         //Comprobar si el id es de un docente
         if(! docenteExiste){
             return res.status(422).json({
@@ -37,7 +46,7 @@ exports.insertarActivacion = async function (req,res,next){
             });
         }
 
-        var grupoExiste = await GrupoService.grupoExiste(req.body.grupo);
+        var grupoExiste = await GrupoService.grupoExiste(idGrupo);
          //Comprobar si el id del grupo existe
          if(! grupoExiste){
             return res.status(422).json({
@@ -45,7 +54,7 @@ exports.insertarActivacion = async function (req,res,next){
             });
         }
 
-        var gradoExiste = await GradoService.gradoExiste(req.body.grado);
+        var gradoExiste = await GradoService.gradoExiste(idGrado);
          //Comprobar si el id del grado existe
          if(! gradoExiste){
             return res.status(422).json({
@@ -53,7 +62,7 @@ exports.insertarActivacion = async function (req,res,next){
             });
         }
 
-        var asignaturaExiste = await AsignaturaService.asignaturaExiste(req.body.asignatura);
+        var asignaturaExiste = await AsignaturaService.asignaturaExiste(idAsignatura);
          //Comprobar si el id de la asignatura existe
          if(! asignaturaExiste){
             return res.status(422).json({
@@ -61,7 +70,7 @@ exports.insertarActivacion = async function (req,res,next){
             });
         }
 
-        var idCamapñaEsInt = HelperNumeric.isNumeric(req.body.campaña);
+        var idCamapñaEsInt = HelperNumeric.isNumeric(idCampaña);
         //Comprobar si el id de la campaña es un numero
         if (!idCamapñaEsInt){
             return res.status(422).json({
@@ -69,7 +78,7 @@ exports.insertarActivacion = async function (req,res,next){
             });
         }
 
-        var campañaExiste = await CampañaService.campañaExiste(req.body.campaña);
+        var campañaExiste = await CampañaService.campañaExiste(idCampaña);
          //Comprobar si el id de la campaña existe
          if(! campañaExiste){
             return res.status(422).json({
@@ -77,7 +86,7 @@ exports.insertarActivacion = async function (req,res,next){
             });
         }
 
-        var noFechaValida = (!moment(req.body.fechaActIni, 'YYYY-MM-DD',true).isValid() || !moment(req.body.fechaActFin, 'YYYY-MM-DD',true).isValid());
+        var noFechaValida = (!moment(fechaActIni, 'YYYY-MM-DD',true).isValid() || !moment(fechaActFin, 'YYYY-MM-DD',true).isValid());
          //Comprobar si el formato de fechas es correcto
          if(noFechaValida){
             return res.status(422).json({
@@ -85,13 +94,13 @@ exports.insertarActivacion = async function (req,res,next){
             });
         }
 
-        if (req.body.fechaActFin <=  req.body.fechaActIni){
+        if (fechaActFin <=  fechaActIni){
             return res.status(422).json({
                 message: "La fecha final debe ser mínimo un día mas tarde que la incial",
             });
         }
 
-        var insertado = await ActivacionService.insertarActivacion(req.body.docente, req.body.grupo, req.body.grado, req.body.asignatura, req.body.campaña, req.body.fechaActIni, req.body.fechaActFin); //Insertar actibación
+        var insertado = await ActivacionService.insertarActivacion(idDocente, idGrupo, idGrado, idAsignatura, idCampaña, fechaActIni, fechaActFin); //Insertar activación
 
         //Comprobar si se ha insertado la activacion
         if(insertado){
@@ -114,71 +123,31 @@ exports.insertarActivacion = async function (req,res,next){
 exports.actualizarActivacion = async function (req,res,next){
     try{
 
-        var activacionExiste = await ActivacionService.activacionExiste(req.body.docente, req.body.grupo, req.body.grado, req.body.asignatura, req.body.campaña);
+        var idDocente = req.body.idDocente;
+        var idGrupo = req.body.idGrupo;
+        var idGrado = req.body.idGrado;
+        var idAsignatura = req.body.idAsignatura;
+        var idCampaña = req.body.idCampaña;
+        var fechaActIni = req.body.fechaActIni;
+        var fechaActFin = req.body.fechaActFin;
+
+        var activacionExiste = await ActivacionService.activacionExiste(idDocente, idGrupo, idGrado, idAsignatura, idCampaña);
         //Comprobar si la activacion existe
         if(!activacionExiste){
             return res.status(201).json({
                 message: "La activación que se intenta actualizar no existe",
             });
         }
-
-        var usuarioExiste = await UsuarioService.usuarioExiste(req.body.docente);
-        //Comprobar si el usuario existe
-        if(! usuarioExiste){
-            return res.status(201).json({
-                message: "El docente seleccionado no existe",
-            });
-        }
-       
-        var docenteExiste = await UsuarioService.docenteExiste(req.body.docente)
-        //Comprobar si el id es de un docente
-        if(! docenteExiste){
-            return res.status(422).json({
-                message: "El docente seleccionado no corresponde a ningún docente existente",
-            });
-        }
-
-        var grupoExiste = await GrupoService.grupoExiste(req.body.grupo);
-         //Comprobar si el id del grupo existe
-         if(! grupoExiste){
-            return res.status(422).json({
-                message: "El grupo seleccionado no corresponde a ningún grupo existente",
-            });
-        }
-
-        var gradoExiste = await GradoService.gradoExiste(req.body.grado);
-         //Comprobar si el id del grado existe
-         if(! gradoExiste){
-            return res.status(422).json({
-                message: "El grado seleccionado no corresponde a ningún grado existente",
-            });
-        }
-
-        var asignaturaExiste = await AsignaturaService.asignaturaExiste(req.body.asignatura);
-         //Comprobar si el id de la asignatura existe
-         if(! asignaturaExiste){
-            return res.status(422).json({
-                message: "La asignatura seleccionada no corresponde a ninguna asignatura existente",
-            });
-        }
-
-        var idCamapñaEsInt = HelperNumeric.isNumeric(req.body.campaña);
+        
+        var idCamapñaEsInt = HelperNumeric.isNumeric(idCampaña);
         //Comprobar si el id de la campaña es un numero
         if (!idCamapñaEsInt){
             return res.status(422).json({
                 message: "La campaña seleccionada no es un número",
             });
-        }
+        }       
 
-        var campañaExiste = await CampañaService.campañaExiste(req.body.campaña);
-         //Comprobar si el id de la campaña existe
-         if(! campañaExiste){
-            return res.status(422).json({
-                message: "La campaña seleccionada no corresponde a ninguna campaña existente",
-            });
-        }
-
-        var noFechaValida = (!moment(req.body.fechaActIni, 'YYYY-MM-DD',true).isValid() || !moment(req.body.fechaActFin, 'YYYY-MM-DD',true).isValid());
+        var noFechaValida = (!moment(fechaActIni, 'YYYY-MM-DD',true).isValid() || !moment(fechaActFin, 'YYYY-MM-DD',true).isValid());
          //Comprobar si el formato de fechas es correcto
          if(noFechaValida){
             return res.status(422).json({
@@ -186,13 +155,13 @@ exports.actualizarActivacion = async function (req,res,next){
             });
         }
 
-        if (req.body.fechaActFin <=  req.body.fechaActIni){
+        if (fechaActFin <=  fechaActIni){
             return res.status(422).json({
                 message: "La fecha final debe ser mínimo un día mas tarde que la incial",
             });
         }
 
-        var actualizado = await ActivacionService.actualizarActivacion(req.body.docente, req.body.grupo, req.body.grado, req.body.asignatura, req.body.campaña, req.body.fechaActIni, req.body.fechaActFin); //Insertar actibación
+        var actualizado = await ActivacionService.actualizarActivacion(idDocente, idGrupo, idGrado, idAsignatura, idCampaña, fechaActIni, fechaActFin); //Insertar activación
 
         //Comprobar si se ha actualizado la activacion
         if(actualizado){
